f521d439378dce26e87744a5cf1eae05eb078e72
Deepam 5708 <shahdeepam96@gmail.com>
2025-09-06 12:13:59 +0530

    Fixed import error issue of 'get_cached_prediction' in model_service.py

--- app/services/model_service.py
+++ app/services/model_service.py
@@ -1,13 +1,13 @@
 import joblib
 import pandas as pd
 from app.core.config import settings
-from app.cache.redis_cache import set_cached_prediction, get_cached_predicted
+from app.cache.redis_cache import set_cached_prediction, get_cached_prediction
 
 model = joblib.load(settings.MODEL_PATH)
 
 def predict_car_price(data: dict):
     cache_key = " ".join([str(val) for val in data.values()])
-    cached = get_cached_predicted(cache_key)
+    cached = get_cached_prediction(cache_key)
     if cached:
         return cached
     
